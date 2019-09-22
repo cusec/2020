d5a4df31621ac0c7c1f1a91cdd91532709c1a0b1
ssial <ssial@uoguelph.ca>
2019-09-22 13:25:02 -0400

    added view map button

--- src/pages/venue.js
+++ src/pages/venue.js
@@ -27,9 +27,11 @@ export default class Venue extends React.Component {
                                 <p><b>Students:</b> Your room bookings will only be done through your school’s head delegate. Contact them for more information.</p>
                                 <p><b>Professionals:</b> CUSEC group rates start at $60/night. Use code ___ during booking to recieve disctounted price. </p>
                                 <p>Contact info@cusec.net if you have any questions.</p>
-                                <div style={{ textAlign: "center" }}>
+                                <div style={{ display: "flex", justifyContent: "center" }}>
                                     <a href="http://hotelbonaventure.com/en" target="_blank"><Button className="button" variant="small" >
                                         Book Room</Button></a>
+                                    <a href="https://www.google.com/maps/place/Hotel+Bonaventure+Montr%C3%A9al/@45.4993335,-73.5649466,15z/data=!4m8!3m7!1s0x0:0x5c249de257b318c0!5m2!4m1!1i2!8m2!3d45.4993335!4d-73.5649466" target="_blank"><Button className="button" variant="small" >
+                                        View Map</Button></a>
                                 </div>
                             </div>
                         </div>
