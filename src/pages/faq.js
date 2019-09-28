9142cbe9f8e81046eb220f003d3d60f2c6f08b1e
ssial <ssial@uoguelph.ca>
2019-09-27 22:54:09 -0400

    updating faqs

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -23,15 +23,19 @@ const FAQList = {
         },
         {
             question: "How can I get a ticket?",
-            answer: "The schedule will be available on the website soon along with our list of speakers and sponsors! We are working around the clock to create the best conference experience for you, promised. "
+            answer: "This year we are carrying out an application process. Filling out this application does not guarantee a spot at CUSEC 2020. You will have the opportunity to purchase your ticket for $60 if you are accepted. Find out more on our Facebook page."
+        },
+        {
+            question: "What is the difference between a student ticket & a professional ticket?",
+            answer: "If you are still registered as a student at an education institution and have not graduated by January 2020, you can buy a student ticket. Otherwise, the professional ticket is for you! "
         },
         {
             question: "When do applications open?",
-            answer: "The schedule will be available on the website soon along with our list of speakers and sponsors! We are working around the clock to create the best conference experience for you, promised. "
+            answer: "Applications are already open. Stayed updated on our Facebook page."
         },
         {
             question: "When do applications close?",
-            answer: "The schedule will be available on the website soon along with our list of speakers and sponsors! We are working around the clock to create the best conference experience for you, promised. "
+            answer: "Applications will be accepted in TWO epochs. Early bird applications close October 12 (23:59 EST), while the final deadline will be October 26 (23:59 EST). If you are from University of New Brunswick, you must submit your application by the early bird deadline."
         },
         {
             question: "Does submitting an application guarantee me a spot?",
@@ -39,7 +43,11 @@ const FAQList = {
         },
         {
             question: "When will I hear back about my application?",
-            answer: "The schedule will be available on the website soon along with our list of speakers and sponsors! We are working around the clock to create the best conference experience for you, promised. "
+            answer: "We'll let you know whether or not your application is accepted as soon as we can. If you applied before the early bird deadline you will hear back sooner! "
+        },
+        {
+            question: "How can I get a refund? Can I re-sell my ticket?",
+            answer: "It is strictly forbidden to re-sell a CUSEC ticket above the purchased price. If you cannot attend anymore, reach out to your Head Delegate or through us at info@cusec.net and we will do our best to assist you."
         },
     ],
     transportation: [
@@ -49,7 +57,7 @@ const FAQList = {
         },
         {
             question: "How do I book my hotel room? How much does it cost?",
-            answer: "The schedule will be available on the website soon along with our list of speakers and sponsors! We are working around the clock to create the best conference experience for you, promised. "
+            answer: "More information will be provided if your application is accepted. Feel free to also contact your Head Delegate around this time for more information. "
         },
 
     ]
@@ -69,6 +77,7 @@ export default class FAQ extends React.Component {
                         <div className="faq-section">
                             {
                                 FAQList.logistics.map((faq, index) => {
+                                    if(faq.question=="")
                                     return (
                                         <div>
                                             <p className="question">{faq.question}</p>
