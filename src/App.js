94d65821ad1a843fa586a18325f0a59c81e77d16
ssial <ssial@uoguelph.ca>
2019-12-16 04:14:01 -0500

    removed app.js warnings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import Landing from "./pages/landing";
 import About from "./pages/about";
 import Venue from "./pages/venue";
 import Contact from "./pages/contact";
 import FAQ from "./pages/faq";
-import Events from "./pages/events";
 import Footer from "./components/footer";
-import NavMenu from "./components/navbar";
-import ScrollableAnchor, { configureAnchors } from "react-scrollable-anchor";
 import './App.css';
 
 
@@ -17,7 +13,6 @@ function App() {
     <div className="App">
       <Landing />
       <About />
-       
       <Venue />
       <Contact />
       <FAQ />
