3b5ed7277c2bfb54e54076d0c603bead9ba149ac
ssial <ssial@uoguelph.ca>
2019-12-22 00:26:34 -0500

    removed events link

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -14,7 +14,7 @@ export default class NavMenu extends React.Component {
                     <Nav >
                         <Nav.Link href="/#about">About</Nav.Link>
                         <NavDropdown title="Conference" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/events">Events</NavDropdown.Item>
+                            <NavDropdown.Item >Coming Soon</NavDropdown.Item>
                         </NavDropdown>
                         <Nav.Link href="/#venue">Venue</Nav.Link>
                         <Nav.Link href="/#contact">Contact</Nav.Link>
