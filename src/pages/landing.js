e3a21fb32af3f49928232b7ac710845c806fbdf9
ssial <ssial@uoguelph.ca>
2019-12-24 00:39:34 -0500

    added ticket button

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from "../assets/images/cusec-logo-dark-background.png";
 import bg from "../assets/images/landing-bg.png";
+import Button from '@material-ui/core/Button';
 import ScrollableAnchor from "react-scrollable-anchor";
 import "../styles/landing.css";
 
@@ -9,35 +10,37 @@ export default class Landing extends React.Component {
     return (
       <React.Fragment>
         <ScrollableAnchor id={"home"}>
-        <div id="container">
-          <div id="landing">
-            <div id="landing-content">
-              <img id="logo" src={Logo} alt="CUSEC 2020" />
-              <div>
-                <h1 className="typewriter-text">> Canadian University Software Engineering Conference</h1>
-              </div>
-              <div className="date-location">
-                <span>
-                  January 17 - 19, 2020
+          <div id="container">
+            <div id="landing">
+              <div id="landing-content">
+                <img id="logo" src={Logo} alt="CUSEC 2020" />
+                <div>
+                  <h1 className="typewriter-text">> Canadian University Software Engineering Conference</h1>
+                </div>
+                <div className="date-location">
+                  <span>
+                    January 17 - 19, 2020
                 </span>
-                <div></div>
-                <span>
-                  Montreal, Canada
+                  <div></div>
+                  <span>
+                    Montreal, Canada
                 </span>
-              </div>
 
+                </div>
+                <a><Button href="https://guestli.st/625575" className="button" variant="small" >
+                  Buy Tickets!</Button></a>
+              </div>
+              <div className="triangle-right"></div>
+              <div className="triangle-left"></div>
+              <img id="landing-bg" src={bg} alt="triangle background" />
             </div>
-            <div className="triangle-right"></div>
-            <div className="triangle-left"></div>
-            <img id="landing-bg" src={bg} alt="triangle background" />
-          </div>
-          <div id="landing-bottom">
-            <div><div className="numbers">01</div><span>Connect</span> with <br />like-minded peers.</div>
-            <div><div className="numbers">02</div><span>Learn</span> from world-renowned <br />experts.</div>
-            <div><div className="numbers">03</div><span>Discover</span> career <br />opportunities.</div>
-          </div>
+            <div id="landing-bottom">
+              <div><div className="numbers">01</div><span>Connect</span> with <br />like-minded peers.</div>
+              <div><div className="numbers">02</div><span>Learn</span> from world-renowned <br />experts.</div>
+              <div><div className="numbers">03</div><span>Discover</span> career <br />opportunities.</div>
+            </div>
 
-        </div>
+          </div>
         </ScrollableAnchor>
       </React.Fragment>
     )
