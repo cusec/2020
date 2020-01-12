a08a78a4c400b522fae6662d6516c4f1662f1a86
ssial <ssial@uoguelph.ca>
2020-01-11 19:26:41 -0500

    adding all links for sponsors

--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -23,27 +23,31 @@ export default class Sponsors extends React.Component {
                     <div className="venue-conatiner">
                         <div className="center-container">
                             <h1>Sponsors</h1>
-                            <div style={{ display: "flex", justifyContent: "center" }}><img className="partner" alt="Kinaxis-Partner" src={Kinaxis} /></div>
-                            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexWrap: "wrap" }}><img className="gold-height" alt="Shopify-Gold" src={shopify} />
-                                <img className="gold-width" alt="RBC-Gold" src={RBC} />
-                                <img className="gold-height" alt="Wish-Gold" src={wish} />
-                                <img className="gold-height" alt="Morgan_Stanley-Gold" src={MS} />
-                                <img className="gold-width" alt="CSE-Gold" src={CSE} />
-                                <img className="gold-width" alt="CAE-Gold" src={CAE} />
+                            <div style={{ display: "flex", justifyContent: "center" }}>
+                            <a href = "https://www.kinaxis.com/en" target="_blank" rel="noopener noreferrer"><img className="partner" alt="Kinaxis-Partner" src={Kinaxis} /></a>
                             </div>
-                            <br/><br/>
-                            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexWrap: "wrap" }}><img className="silver-width" alt="TripAdvisor-Silver" src={TripAdvisor} />
-                                <img className="silver-height" alt="Amazon-Silver" src={Amazon} />
-                                <img className="silver-height" alt="Fellow-Silver" src={Fellow} />
-                                <img className="silver-width" alt="diff-Silver" src={diff} />
+                            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexWrap: "wrap" }}>
+                                <a href="https://www.shopify.ca/" target="_blank" rel="noopener noreferrer"><img className="gold-height" alt="Shopify-Gold" src={shopify} /></a>
+                               <a href="https://www.rbcroyalbank.com/personal.html" target="_blank" rel="noopener noreferrer"> <img className="gold-width" alt="RBC-Gold" src={RBC} /></a>
+                                <a href="https://www.wish.com/?&hide_login_modal=true" target="_blank" rel="noopener noreferrer"><img className="gold-height" alt="Wish-Gold" src={wish} /></a>
+                                <a href="https://www.morganstanley.com/" target="_blank" rel="noopener noreferrer"><img className="gold-height" alt="Morgan_Stanley-Gold" src={MS} /></a>
+                                <a href="https://www.cse-cst.gc.ca/careers-carrieres" target="_blank" rel="noopener noreferrer"><img className="gold-width" alt="CSE-Gold" src={CSE} /></a>
+                                <a href="https://www.cae.com/" target="_blank" rel="noopener noreferrer"><img className="gold-width" alt="CAE-Gold" src={CAE} /></a>
+                            </div>
+                            <br /><br />
+                            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexWrap: "wrap" }}>
+                            <a href="https://www.tripadvisor.ca/" target="_blank" rel="noopener noreferrer"><img className="silver-width" alt="TripAdvisor-Silver" src={TripAdvisor} /></a>
+                                <a href="https://www.amazon.ca/" target="_blank" rel="noopener noreferrer"><img className="silver-height" alt="Amazon-Silver" src={Amazon} /></a>
+                                <a href="https://www.fellow.app/" target="_blank" rel="noopener noreferrer"><img className="silver-height" alt="Fellow-Silver" src={Fellow} /></a>
+                                <a href="https://www.diffagency.com/" target="_blank" rel="noopener noreferrer"><img className="silver-width" alt="diff-Silver" src={diff} /></a>
                                 <img className="silver-height" alt="knox-Silver" src={knox} />
                             </div>
-                            <br/><br/>
+                            <br /><br />
                             <div style={{ display: "flex", justifyContent: "center", alignItems: "center", flexWrap: "wrap" }}>
-                            <img className="friend-height" alt="1Password-Friend" src={Pass} />
-                            <a href="http://www.stickermule.com/supports/cusec-2020" target="_blank">
-                                <img className="friend-height" alt="Stickermule-Friend" src={Stickermule} />
-                            </a>
+                                <a href="https://1password.com/" target="_blank" rel="noopener noreferrer"><img className="friend-height" alt="1Password-Friend" src={Pass} /></a>
+                                <a href="http://www.stickermule.com/supports/cusec-2020" target="_blank" rel="noopener noreferrer">
+                                    <img className="friend-height" alt="Stickermule-Friend" src={Stickermule} />
+                                </a>
                             </div>
                         </div>
                     </div>
