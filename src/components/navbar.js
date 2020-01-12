9a80348d0044e61fbeefe35bd4e3a6a4d6fe1274
ssial <ssial@uoguelph.ca>
2020-01-11 20:39:42 -0500

    removing blah block

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,17 +12,6 @@ export default class NavMenu extends React.Component {
                 <Navbar.Brand href="/#home">CUSEC 2020</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav" style={{ justifyContent: "flex-end" }}>
-                    {/* <Nav >
-                        <Nav.Link href="/#about">About</Nav.Link>
-                      
-                        <Nav.Link href="/#venue">Venue</Nav.Link>
-                        <Nav.Link href="/#sponsors">Sponsors</Nav.Link>
-                        <Nav.Link href="/#contact">Contact</Nav.Link>
-                        <Nav.Link href="/#faq">FAQ</Nav.Link>
-                 
-                        <Nav.Link href="https://2019.cusec.net/">2019</Nav.Link>
-                   
-                    </Nav> */}
                     <div className="navbar-nav">
                         <a href="/#about" role="tab" data-rb-event-key="/#about" tabindex="-1" aria-selected="false" className="nav-link">About</a>
                         <a href="/#venue" role="tab" data-rb-event-key="/#venue" tabindex="-1" aria-selected="false" className="nav-link">Venue</a>
