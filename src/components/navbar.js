1275ab3dc30cb64d904ed5b9ba9ce163ce02c894
ssial <ssial@uoguelph.ca>
2020-01-14 01:49:49 -0500

    link added

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -21,6 +21,7 @@ export default class NavMenu extends React.Component {
                         <Link smooth to="/#sponsors"><div className="nav-link">Sponsors</div></Link>
                         <Link smooth to="/#contact"><div className="nav-link">Contact</div></Link>
                         <Link smooth to="/#faq"><div className="nav-link">FAQ</div></Link>
+                        <Link to="/schedule"><div className="nav-link">Schedule</div></Link>
                         <Link to="/speakers"><div className="nav-link">Speakers</div></Link>
                         <Link to="/events"><div className="nav-link">Events</div></Link>
                         <a href="https://2019.cusec.net/" target="_blank" role="tab" data-rb-event-key="https://2019.cusec.net/" tabindex="-1" aria-selected="false" className="nav-link">2019</a>
--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Table } from 'react-bootstrap';
 
 export default class Schedule extends React.Component {
+    componentDidMount() {
+		window.scrollTo(0, 0);
+	}
     render() {
         return (
             <React.Fragment>
